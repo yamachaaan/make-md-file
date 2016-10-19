20e9c7378c5683746c438fb2dc8af4b1353ab2be
ymzkryo <ymzkryo@ymzkryo-no-MacBook-Air.local>
2016-10-19 13:29:45 +0900

    version1.1.0

--- mmf-d.sh
+++ mmf-d.sh
@@ -3,6 +3,12 @@ echo "何年?"
 read YYYY
 echo "何月?"
 read MM
+if [ ! -d ~/blog.yamachaaan.net/blog/data/$YYYY/$MM/ ]
+then
+    mkdir ~/blog.yamachaaan.net/blog/data/$YYYY/$MM/
+else
+cd ~/blog.yamachaaan.net/blog/data/$YYYY/$MM/
+fi
 cd ~/blog.yamachaaan.net/blog/data/$YYYY/$MM/
 name=`date +%Y-%m-%d`
 touch ~/blog.yamachaaan.net/blog/data/$YYYY/$MM/$name-diary.md
@@ -24,11 +30,16 @@ echo
 echo "## 作ったもの">> "$name-diary.md"
 echo 
 echo "## KPT">> "$name-diary.md"
+echo " ">> "$name-diary.md"
 echo "### K">> "$name-diary.md"
+echo " ">> "$name-diary.md"
 echo "- ">>  "$name-diary.md"
 echo "### P">> "$name-diary.md"
+echo " ">> "$name-diary.md"
 echo "- ">> "$name-diary.md"
 echo "### T">> "$name-diary.md"
+echo " ">> "$name-diary.md"
 echo "- ">> "$name-diary.md"
+echo " ">> "$name-diary.md"
 echo "$name-diary.mdファイルを作成しました。"
 vim ~/blog.yamachaaan.net/blog/data/$YYYY/$MM/$name-diary.md
