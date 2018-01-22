cb3c3c87e6acfb8f85b0829546df5070696bc068
yamachaaan <ymzk.yamachan@gmail.com>
2018-01-22 23:41:34 +0900

    set week date

--- mmf-w.py
+++ mmf-w.py
@@ -7,13 +7,17 @@ Modified: 2018-01-18
 """
 
 import os
-import datetime
+from datetime import datetime,timedelta,date
 
-now = datetime.datetime.now()
+now = datetime.now()
 date = now.strftime("%Y-%m-%d")
 datepath = now.strftime("%Y/%m")
 filename = date + "-diary"
-mainPath = "/Users/yamachaaan/blog.yamachaaan.net/data/"
+#mainPath = "/Users/yamachaaan/blog.yamachaaan.net/data/"
+mainPath = "/users/yamachaaan/mmf/"
+dayList = [0 for Cnt in range(6)]
+weekNo = date(int(now.strftime("%Y")),int(now.strftime("%-m")),int(now.strftime("%-d"))).isocalendar()
+
 def makeFile():
     if (os.path.isdir(mainPath + datepath)):
         pass
@@ -21,11 +25,13 @@ def makeFile():
         os.makedirs(mainPath + datepath)
     os.chdir(mainPath + datepath)
 
+def setweekDay():
+    for dayCnt in range(1,7):
+        d = now-timedelta(dayCnt)
+        dayList.insert(dayCnt,d.strftime("%Y-%m-%d"))
+        # print(dayList[dayCnt])
+
 def createContents():
-    year = now.strftime("%Y")
-    month = now.strftime("%-m")
-    day = now.strftime("%-d")
-    list = datetime.date(int(year),int(month),int(day)).isocalendar()
     f = open(filename + '.md','a')
     f.write("---\n")
     f.write("layout: post\n")
@@ -34,15 +40,26 @@ def createContents():
     f.write("tags: ['review']\n")
     f.write("pagetype: posts\n")
     f.write("---\n")
-    f.write(now.strftime("%Y") + "年-W" + str(list[1]))
+    f.write(now.strftime("%Y") + "年-W" + weekNo[1])
     f.write("  \n")
     f.write("### 目標")
     f.write("  \n")
     f.write("### 記事")
     f.write("  \n")
+    for dayCnt in dayList:
+        f.write("[" + dayCnt + " " + "title" + "]" + "\n")
+
+    f.write("### つくったもの")
+    f.write("  \n")
+    f.write("### よんだもの")
+    f.write("  \n")
+    f.write("### みたもの")
+    f.write("  \n")
+    f.write("### " + now.strftime("%Y") +"年-W" + str(list[1]) + "の目標")
+    f.write("  \n")
     f.close()
 
 if __name__ == '__main__':
     makeFile()
+    setweekDay()
     createContents()
-
