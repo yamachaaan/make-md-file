419baa8133b486436ebaeeb12633d78facde32b6
yamachaaan <ymzk.yamachan@gmail.com>
2018-04-03 22:09:03 +0900

    title,ファイルの生成

--- mmf-m.py
+++ mmf-m.py
@@ -10,23 +10,43 @@ import os
 from datetime import datetime,timedelta,date
 
 mainPath = "/Users/yamachaaan/blog.yamachaaan.net/data/"
+now = datetime.now().strftime("%Y-%m-%d")
 
-def createPath():
-    now = datetime.now().strftime("%Y-%m-%d")
-    fileName = now + "-diary.html"
-    ymdPath = fileName.split("-")
-    fileFullPath = mainPath + ymdPath[0] + "/" + ymdPath[1] + "/" + ymdPath[2] + "/" + fileName
+def createContens():
+    return "hoge"
+
+def getDate(now):
+    SplitYmd = now.split("-")
+    return SplitYmd
+
+def createfileName(now):
+    fileName = now + "-diary.md"
+    return fileName
+
+def createFilePath(ymdPath,fileName):
+    fileFullPath = mainPath + ymdPath[0] + "/" + ymdPath[1] + "/" + fileName
+    return fileFullPath
+
+def setTitle(ymdPath):
+    title = ymdPath[0] + "-" + ymdPath[1] + " ふりかえり"
+    return title
+
+def makeFile(fileFullPath,title,now):
     print(fileFullPath)
-    print(ymdPath[1])
-
-# def setTitle():
-# 
-# def makeFile():
-# 
-# def createContnts():
+    f = open(fileFullPath,'w')
+    f.write("---\n")
+    f.write("layout: post\n")
+    f.write("pubdate: " + now + ":23:59:59+09;00\n")
+    f.write("title: " + title + "\n")
+    f.write("tags: ['diary']\n")
+    f.write("pagetype: posts\n")
+    f.write("---\n")
+    f.write(createContens())
+    f.close()
 
 if __name__ == '__main__':
-    createPath()
-    #setTitle()
-    #makeFile()
-    #createContnts()
+    ymdPath = getDate(now)
+    fileName = createfileName(now)
+    fileFullPath = createFilePath(ymdPath,fileName)
+    title = setTitle(ymdPath)
+    makeFile(fileFullPath,title,now)
