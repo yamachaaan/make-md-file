7cad8fdb5158be23e74ab11481f3895cc2399890
yamachaaan <ymzk.yamachan@gmail.com>
2018-04-16 22:37:22 +0900

    次の処理をコメントで追加

--- mmf.py
+++ mmf.py
@@ -133,25 +133,53 @@ def createFile():
         f.write(headerCont)
     #if ("week" in mmfType) is True:
     if ("week" in mmfType) is True:
-        cnt = 0
+        weekCnt = 0
         for weekCont in weekContent:
-            cnt += 1
-            if cnt == 3:
+            weekCnt += 1
+            if weekCnt == 2:
+                # 週の目標を取得
+            if weekCnt == 3:
                 for Cnt in range(1,8):
                     tmp = str((datetime.now() - timedelta(Cnt)).strftime("%Y-%m-%d"))
                     urlList = tmp.split("-")
                     surl = "/" + urlList[0] + "/" + urlList[1] + "/" + urlList[2] + "/"
                     seturl = "http://blog.yamachaaan.net" + surl + "diary.html"
                     f.write("- [" + tmp + " " + getPostTitle(tmp) + "](" + seturl + ")\n")
+            if weekCnt == 4:
+                # つくったものを取得
+            if weekCnt == 5:
+                # よんだものを取得
+            if weekCnt == 6:
+                # みたものを取得
             f.write(weekCont)
     if ("month" in mmfType) is True:
+        monthCnt = 0
         for monthCont in monthContent:
+            monthCnt += 1
+            if monthCnt == 1:
+                # 月の目標を取得
+            if monthCnt == 2:
+                # 週のふりかえり記事を取得
+            if monthCnt == 3:
+                # つくったものを取得
+            if monthCnt == 4:
+                # よんだものを取得
+            if monthCnt == 5:
+                # みたものを取得
             f.write(monthCont)
     if ("quote" in mmfType) is True:
+        quoteCnt = 0
         for quoteCont in quoteContent:
+            quoteCnt += 1
+            if quoteCnt == 1:
+                # 四半期の目標を取得
             f.write(quoteCont)
     if ("year" in mmfType) is True:
+        yearCnt = 0
         for yearCont in yearContent:
+            yearCnt += 1
+            if yearCnt == 1:
+                # 年の目標を取得
             f.write(yearCont)
     f.close()
 
